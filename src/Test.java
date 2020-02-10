16db8e04bac66ae7055642d5af49af29b154f07d
onurhizlan <onur_hzln@hotmail.com>
2020-02-10 14:53:21 +0300

    JAVA00 test branch

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,11 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Test {
 
     public static void main(String[] args) {
 
         System.out.println("test");
+        System.out.println("test2 ");
     }
 
 }
