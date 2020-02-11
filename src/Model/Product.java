86a6f242df8edd6b0b7b4cec493919a5da9cf1cc
onurhizlan <onur_hzln@hotmail.com>
2020-02-12 00:42:47 +0300

    added getter and setter

--- src/Model/Product.java
+++ src/Model/Product.java
@@ -24,6 +24,38 @@ public class Product {
         this.productPrice = productPrice;
     }
 
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Date getProductDate() {
+        return productDate;
+    }
+
+    public void setProductDate(Date productDate) {
+        this.productDate = productDate;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
