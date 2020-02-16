896d69f3fe7455efef339bcac16be56126b58b37
volkangurbuz <volkangurbuz16@gmail.com>
2020-02-16 13:54:26 +0100

    added post method with one amazon example

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -7,12 +7,19 @@ public class Test {
 
     public static void main(String[] args) {
 
-    String exampleJsonUrl = "https://volkangurbuz.com/restapi/product/readAllStudent.php";
+        String exampleJsonUrl = "https://volkangurbuz.com/restapi/product/readAllStudent.php";
 
-    String getJson = Util2.sendGetRequest(exampleJsonUrl);
+        String getJson = Util2.sendGetRequest(exampleJsonUrl);
 
-     System.out.println("Get Json: " +  getJson);
+       // System.out.println("Get Json: " + getJson);
+
+        String exampleJsonPostUrl = "https://www.amazon.com.tr/xa/dealcontent/v2/GetDeals";
+        String formBody = "{\"requestMetadata\":{\"marketplaceID\":\"A33AVAJ2PDY3EV\",\"clientID\":\"goldbox_mobile_pc\",\"sessionID\":\"259-3784078-1319449\"},\"dealTargets\":[{\"dealID\":\"0e782399\"},{\"dealID\":\"10a84bf1\"},{\"dealID\":\"2cc95013\"},{\"dealID\":\"30b40347\"},{\"dealID\":\"4a06ebd5\"},{\"dealID\":\"55ee3d3f\"},{\"dealID\":\"8e495e40\"},{\"dealID\":\"bb3d3e4b\"},{\"dealID\":\"cffbb963\"},{\"dealID\":\"dd016133\"},{\"dealID\":\"ea7644de\"},{\"dealID\":\"f60ab753\"}],\"customerContext\":{\"languageOfPreference\":\"tr_TR\"}}";
+
+        System.out.println("get post json: " + Util2.sendPostRequest(exampleJsonPostUrl, formBody));
+
 
     }
+
 
 }
--- src/Util/Util2.java
+++ src/Util/Util2.java
@@ -1,6 +1,10 @@
 package Util;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -20,14 +24,41 @@ public final class Util2 {
             HttpResponse<String> response = client.send(request,
                     HttpResponse.BodyHandlers.ofString());
 
-                if(response.statusCode() == 200){
-                    return response.body();
-                }
-        }
-        catch (Exception e){
+            if (response.statusCode() == 200) {
+                return response.body();
+            }
+        } catch (Exception e) {
             System.err.println("Failure , " + e.toString());
         }
         return null;
     }
+
+    public static String sendPostRequest(String URL, String postForm) {
+        String response = null;
+        try {
+            BufferedReader br;
+            StringBuilder sb = new StringBuilder();
+            URL url = new URL(URL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.getOutputStream().write(postForm.getBytes("UTF-8"));
+            con.getInputStream();
+
+            if (200 <= con.getResponseCode() && con.getResponseCode() <= 299) {
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            while ((response = br.readLine()) != null) {
+                sb.append(response);
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            System.err.println("Failure , " + e.toString());
+        }
+        return null;
+    }
+
 
 }
