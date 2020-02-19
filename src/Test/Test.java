fc59dd93c9a2a2d774dfa2309a00f778c09af839
volkangurbuz <volkangurbuz16@gmail.com>
2020-02-19 20:13:10 +0100

    show product name and prices

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -7,6 +7,8 @@ import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.TagNode;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -24,11 +26,25 @@ public class Test {
             XPath xpath = xpf.newXPath();
             String webSiteDoc =  Util2.getURLSource("https://www.amazon.com.tr/gp/browse.html?node=13526049031&ref_=nav_em_T1_0_4_8_3_pca_shaving");
 
+            NodeList nodeList = (NodeList) xpath.evaluate("//li[contains(@id, 'result')]",Util2.wrapToDocument(webSiteDoc) ,
+                    XPathConstants.NODESET);
 
-            Element userElement = (Element) xpath.evaluate("//li[contains(@id, 'result')]",Util2.wrapToDocument(webSiteDoc) ,
-                    XPathConstants.NODE);
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node= nodeList.item(i);
+                if(node instanceof Element){
+                    Element element = (Element) node;
+                    String productName =(String) xpath.evaluate(".//div[3]//div[1]//a//h2",element ,
+                            XPathConstants.STRING);
 
-            System.out.println(userElement.getAttribute("id"));
+                    String productPrice =(String)   xpath.evaluate(".//div[5]//div[1]//a//span[2]",element ,
+                            XPathConstants.STRING);
+
+                    System.out.println("products: "+ productName + " productPrice: " + productPrice) ;
+
+                }
+            }
+
+           // System.out.println(userElement.getAttribute("id"));
 
         } catch (Exception e) {
             e.printStackTrace();
