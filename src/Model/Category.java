639c384ddc0f07201f70921624e8f3444586c400
volkangurbuz <volkangurbuz16@gmail.com>
2020-03-07 14:11:25 +0100

    added category link

--- src/Model/Category.java
+++ src/Model/Category.java
@@ -5,16 +5,19 @@ public class Category {
     private String categoryId;
     private String categoryName;
     private String subCategoryName;
+    private String categoryLink;
 
     public Category(String categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+
     }
 
-    public Category(String categoryId, String categoryName, String subCategoryName) {
+    public Category(String categoryId, String categoryName, String subCategoryName, String categoryLink) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
         this.subCategoryName = subCategoryName;
+        this.categoryLink = categoryLink;
     }
 
     public String getCategoryId() {
@@ -41,12 +44,21 @@ public class Category {
         this.subCategoryName = subCategoryName;
     }
 
+    public String getCategoryLink() {
+        return categoryLink;
+    }
+
+    public void setCategoryLink(String categoryLink) {
+        this.categoryLink = categoryLink;
+    }
+
     @Override
     public String toString() {
         return "Category{" +
                 "categoryId='" + categoryId + '\'' +
                 ", categoryName='" + categoryName + '\'' +
                 ", subCategoryName='" + subCategoryName + '\'' +
+                ", categoryLink='" + categoryLink + '\'' +
                 '}';
     }
 }
