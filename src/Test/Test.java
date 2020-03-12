883daaaf93f0403331b654e5d823dbd4c2b7a242
volkangurbuz <volkangurbuz16@gmail.com>
2020-03-12 22:21:59 +0100

    added mysql and tested on local

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,14 +1,16 @@
 package Test;
 
 import Model.Page;
+import Util.MysqlConnect;
 import Util.Util2;
 
 import java.io.IOException;
+import java.sql.*;
 
 
 public class Test {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
 
         Page pageAmazon = new Page("amazon" , "https://www.amazon.com.tr", "https://www.amazon.com.tr/gp/site-directory?ref_=nav_em_T1_0_2_2_12__fullstore");
         Util2 testUtil = new Util2();
@@ -17,12 +19,30 @@ public class Test {
        // testUtil.parsePage(pageAmazon);
 
 
-        String webSiteDoc = Util2.getURLSource("https://www.zirvemotomotiv.com.tr");
+        //String webSiteDoc = Util2.getURLSource("https://www.zirvemotomotiv.com.tr");
 
-        Util2.wrapToDocument(webSiteDoc);
+       // Util2.wrapToDocument(webSiteDoc);
 
        // pageAmazon.showAllCategories();
 
+        MysqlConnect mysqlConnect = new MysqlConnect();
+
+
+        String sql = "SELECT * FROM `product` ";
+        try {
+            Statement stmt = mysqlConnect.connect().createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                String s = rs.getString("product_name");
+                System.out.println(s);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            mysqlConnect.disconnect();
+        }
 
 
 
