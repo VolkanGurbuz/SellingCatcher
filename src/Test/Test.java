6559cbf8faf9cfd56acf9b84388ff8d29e6b1ee9
Volkan Gürbüz <volkangurbuz16@gmail.com>
2020-04-15 13:08:41 +0200

    Update Test.java

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -14,8 +14,8 @@ import java.util.ArrayList;
 public class Test {
 
     public static void main(String[] args) throws IOException, SQLException {
-
-        Page pageAmazon = new Page("amazon" , "https://www.amazon.com.tr", "https://www.amazon.com.tr/gp/site-directory?ref_=nav_em_T1_0_2_2_12__fullstore");
+        String url = "";
+        Page pageAmazon = new Page("amazon" , url, "url/fullstore");
         Util2 testUtil = new Util2();
         MysqlConnect mysqlConnect = new MysqlConnect();
         DatabaseOperations dboperations = new DatabaseOperations(mysqlConnect);
@@ -29,14 +29,6 @@ public class Test {
         testUtil.parsePage(pageAmazon);
 
     dboperations.addProductsToServer(testUtil.getProductList());
-
-
-        //String webSiteDoc = Util2.getURLSource("https://www.zirvemotomotiv.com.tr");
-
-       // Util2.wrapToDocument(webSiteDoc);
-
-       // pageAmazon.showAllCategories();
-
 
 
 
